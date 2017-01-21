13f97bafb6dced3f8e3198983253b1490b1703a5
Lukas Velikov <velikovlukas+github@gmail.com>
2017-01-20 19:29:20 -0500

    Add files via upload

---
+++ Driving Distance/index.js
@@ -0,0 +1,154 @@
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+var placesList = [];
+
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.8688, lng: 151.2195},
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        // markers.forEach(function(marker) {
+        //     marker.setMap(null);
+        // });
+        // markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+
+            placesList.push(place);
+            displayPlaceL(placesList)
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+}
+
+function displayPlaceL(places){
+    places.forEach(function(place) {
+       window.alert(place.geometry.location);
+    });
+    calcDistances(places);
+}
+
+function calcDistances(places){
+    if(places.length > 1) {
+        var tuples = [];
+        var origin = {lat: parseFloat(places[0].geometry.location.lat()), lng: parseFloat(places[0].geometry.location.lng())};
+
+        for(var i=1; i<places.length; i++) {
+            tuples[i-1] = {lat: parseFloat(places[i].geometry.location.lat()), lng: parseFloat(places[i].geometry.location.lng())};
+        }
+
+        var geocoder = new google.maps.Geocoder;
+        var service = new google.maps.DistanceMatrixService;
+        var destinationIcon = 'https://chart.googleapis.com/chart?' +
+            'chst=d_map_pin_letter&chld=D|FF0000|000000';
+        var originIcon = 'https://chart.googleapis.com/chart?' +
+            'chst=d_map_pin_letter&chld=O|FFFF00|000000';
+        var bounds = new google.maps.LatLngBounds;
+
+        service.getDistanceMatrix({
+            origins: [origin],
+            destinations: tuples,
+            travelMode: 'DRIVING',
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false
+        }, function (response, status) {
+            if (status !== 'OK') {
+                alert('Error was: ' + status);
+            } else {
+                var originList = response.originAddresses;
+                var destinationList = response.destinationAddresses;
+                var outputDiv = document.getElementById('output');
+                outputDiv.innerHTML = '';
+
+
+                var showGeocodedAddressOnMap = function (asDestination) {
+                    var icon = asDestination ? destinationIcon : originIcon;
+                    return function (results, status) {
+                        if (status === 'OK') {
+                            //map.fitBounds(bounds.extend(results[0].geometry.location));
+                            // markersArray.push(new google.maps.Marker({
+                            //     map: map,
+                            //     position: results[0].geometry.location,
+                            //     icon: icon
+                            // }));
+                        } else {
+                            alert('Geocode was not successful due to: ' + status);
+                        }
+                    };
+                };
+
+                for (var i = 0; i < originList.length; i++) {
+                    var results = response.rows[i].elements;
+                    geocoder.geocode({'address': originList[i]},
+                        showGeocodedAddressOnMap(false));
+                    for (var j = 0; j < results.length; j++) {
+                        geocoder.geocode({'address': destinationList[j]},
+                            showGeocodedAddressOnMap(true));
+                        outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+                            ': ' + results[j].distance.text + ' in ' +
+                            results[j].duration.text + '<br>';
+                    }
+                }
+            }
+        });
+    }
+}
+
